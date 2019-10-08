cd0d12645ba7cb359284fb480b4c436331bfeed1
Ye-sun12 <56026416+Ye-sun12@users.noreply.github.com>
2019-10-08 15:05:20 +0900

    Update selection.py

--- selection.py
+++ selection.py
@@ -2,15 +2,11 @@ def selectionSort(A, n):
     for last in range(n,0,-1):
         max = last
         b = A[last]
-        print(max,"max")
         for num in range(last-1,-1,-1):
             if A[num] > A[max]:
                 max = num
-        print(max,"max")
         A[last] = A[max]
-        print(b,"b")
         A[max] = b
-        print(A)
     return A
 
 a = [2,5,3,1]
