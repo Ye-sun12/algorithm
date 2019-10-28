5ae67e58c488c59a20c329c097a858f996186312
Lenovo <ays0630@gmail.com>
2019-10-28 17:44:39 +0900

    six commit

--- selection.py
+++ selection.py
@@ -5,18 +5,15 @@ def selectionSort(A, n):
         for num in range(last-1,-1,-1):
             if A[num] > A[max]:
                 max = num
-<<<<<<< HEAD
-        A[last] = A[max]        
+            A[last] = A[max]        
         A[max] = b        
     return A
 
 a = [2,5,3,1]
 print(selectionSort(a,len(a)-1))
-=======
         A[last] = A[max]
         A[max] = b
     return A
 
 a = [2,5,3,1]
 print(selectionSort(a,3))
->>>>>>> cd0d12645ba7cb359284fb480b4c436331bfeed1
